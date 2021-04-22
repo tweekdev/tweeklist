cac98ef735f0af98295a29c80c14d5d0b232dc10
tweekdev <tja.theau54@gmail.com>
2021-04-22 17:57:28 +0200

    Fetching Data (getStaticProps)

--- pages/tweeklist/index.js
+++ pages/tweeklist/index.js
@@ -1,7 +1,24 @@
-const Tweeklist = () => {
+import styles from '../../styles/Tweeks.module.css';
+export const getStaticProps = async () => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await response.json();
+
+  return {
+    props: { tweeks: data },
+  };
+};
+
+const Tweeklist = ({ tweeks }) => {
   return (
     <div>
-      <h1>All Tweek</h1>
+      <h1>All Tweeks</h1>
+      {tweeks.map((tweek) => (
+        <div key={tweek.id}>
+          <a className={styles.single}>
+            <h3>{tweek.name}</h3>
+          </a>
+        </div>
+      ))}
     </div>
   );
 };
