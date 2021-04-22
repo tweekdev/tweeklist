51c3f84f93bbe1a9e55296999668ae4dad30013a
tweekdev <tja.theau54@gmail.com>
2021-04-22 18:13:49 +0200

    Dynamic Routes (part 2 - getStaticPaths)

--- pages/tweeklist/[id].js
+++ pages/tweeklist/[id].js
@@ -1,3 +1,19 @@
+export const getStaticPaths = async () => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await response.json();
+
+  const paths = data.map((tweek) => {
+    return {
+      params: { id: tweek.id.toString() },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
 const Details = () => {
   return (
     <div>
